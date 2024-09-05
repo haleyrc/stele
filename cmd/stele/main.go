8215eccb2b64f90e15c2d374773daa90e56d258c
Ryan Haley <me@ryanchaley.com>
2024-09-05 10:36:33 -0400

    Add a version command

--- cmd/stele/main.go
+++ cmd/stele/main.go
@@ -12,6 +12,12 @@ import (
 	"github.com/haleyrc/stele"
 )
 
+const (
+	commit  = ""
+	date    = ""
+	version = ""
+)
+
 func main() {
 	ctx := context.Background()
 
@@ -29,6 +35,9 @@ func main() {
 	case "help":
 		printUsage()
 		os.Exit(0)
+	case "version":
+		printVersion()
+		os.Exit(0)
 	default:
 		printUsage()
 		os.Exit(1)
@@ -38,6 +47,15 @@ func main() {
 func printUsage() {
 	fmt.Fprintln(os.Stderr, strings.TrimSpace(usage))
 	fmt.Fprintln(os.Stderr)
+}
+
+func printVersion() {
+	fmt.Fprint(os.Stderr, logo)
+	fmt.Fprintln(os.Stderr, "stele: A no frills blogging platform for people with analysis paralysis")
+	fmt.Fprintln(os.Stderr)
+	fmt.Fprintln(os.Stderr, "Commit: ", commit)
+	fmt.Fprintln(os.Stderr, "Date:   ", date)
+	fmt.Fprintln(os.Stderr, "Version:", version)
 }
 
 func runBuild(ctx context.Context) {
@@ -87,6 +105,15 @@ func exitWithError(err error) {
 	log.Println("ERR:", err)
 	os.Exit(1)
 }
+
+const logo = `
+     _       _
+    | |     | |
+ ___| |_ ___| | ___
+/ __| __/ _ \ |/ _ \
+\__ \ ||  __/ |  __/
+|___/\__\___|_|\___|
+`
 
 const usage = `
 A no frills blogging platform for people with analysis paralysis.
